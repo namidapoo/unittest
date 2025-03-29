153d7940f8f76fd1cf4f894b18aed85078b6e0e4
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-03-30 03:47:09 +0900

    docs: add commment

--- src/04/06/index.test.ts
+++ src/04/06/index.test.ts
@@ -38,10 +38,13 @@ test("バリデーションに成功した場合、成功レスポンスが返�
   // バリデーションに通過する入力値を用意
   const input = inputFactory();
   // 入力値を含んだ成功レスポンスが返るよう、モックを施す
+  // この処理をしたおかげで、この後`postMyArticle`関数を呼び出すときにモック関数が呼び出されるようになる
   const mock = mockPostMyArticle(input);
   // テスト対象の関数に、input を与えて実行
   const data = await postMyArticle(input);
   // 取得したデータに、入力内容が含まれているかを検証
+  // L42でモック関数を呼び出したおかげで、このテストではモック関数が呼び出されている
+  // L42の処理がなければ単にL7でモックしただけなので`undefined`が返却される
   expect(data).toMatchObject(expect.objectContaining(input));
   // モック関数が呼び出されたかを検証
   expect(mock).toHaveBeenCalled();
@@ -53,6 +56,8 @@ test("バリデーションに失敗した場合、reject される", async () =
   const input = inputFactory({ title: "", body: "" });
   // 入力値を含んだ成功レスポンスが返るよう、モックを施す
   const mock = mockPostMyArticle(input);
+  // L58によってこのモジュールの中でモックが施されたので`postMyArticle`関数を呼び出すときにはモック関数が呼び出される
+  // L58の処理がなければ単にL7でモックしただけなので`undefined`が返却される
   // バリデーションに通過せず reject されるかを検証
   await postMyArticle(input).catch((err) => {
     // エラーオブジェクトをもって reject されたことを検証

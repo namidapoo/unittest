86683ecba0eb17c9cd4066c716b8c037060a438e
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-03-29 17:59:32 +0900

    docs: add comment

--- src/04/03/index.test.ts
+++ src/04/03/index.test.ts
@@ -12,6 +12,8 @@ describe("getGreet", () => {
       email: "taroyamada@myapi.testing.com",
     });
     await expect(getGreet()).resolves.toBe("Hello, anonymous user!");
+    // 以下でも同義
+    // expect(await getGreet()).toBe("Hello, anonymous user!");
   });
   test("データ取得成功時：ユーザー名がある場合", async () => {
     jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
@@ -20,7 +22,10 @@ describe("getGreet", () => {
       name: "taroyamada",
     });
     await expect(getGreet()).resolves.toBe("Hello, taroyamada!");
+    // 以下でも同義
+    // expect(await getGreet()).toBe("Hello, taroyamada!");
   });
+  // データ取得失敗時に関連コードがどのように振る舞うかをテストするという観点
   test("データ取得失敗時", async () => {
     // getMyProfile が reject した時の値を再現
     jest.spyOn(Fetchers, "getMyProfile").mockRejectedValueOnce(httpError);
@@ -28,6 +33,7 @@ describe("getGreet", () => {
       err: { message: "internal server error" },
     });
   });
+  // データ取得失敗時に例外がスローされているかをテストするという観点
   test("データ取得失敗時、エラー相当のデータが例外としてスローされる", async () => {
     expect.assertions(1);
     jest.spyOn(Fetchers, "getMyProfile").mockRejectedValueOnce(httpError);
